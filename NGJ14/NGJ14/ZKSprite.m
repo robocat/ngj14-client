2744a3b8fd7ffd7c8f0e4ca59729fe63f743d278
Willi Wu <willi@robo.cat>
2014-02-15 15:54:04 +0100

    Sprite sheet

---
+++ NGJ14/NGJ14/ZKSprite.m
@@ -0,0 +1,13 @@
+//
+//  ZKPerson.m
+//  NGJ14
+//
+//  Created by Willi Wu on 15/02/14.
+//  Copyright (c) 2014 Willi Wu. All rights reserved.
+//
+
+#import "ZKPerson.h"
+
+@implementation ZKPerson
+
+@end
