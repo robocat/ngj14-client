e3334907c3723bfeb0ae57b96bf6a52af3820e75
Ulrik Flænø Damm <ulrikdamm@me.com>
2014-02-15 20:37:00 +0100

    The guy can walk around but no longer moonwalk oh well

--- NGJ14/NGJ14/ZKPerson.m
+++ NGJ14/NGJ14/ZKPerson.m
@@ -9,6 +9,7 @@
 #import "ZKPerson.h"
 
 #define MOVE_ACTION @"MOVE_ACTION"
+#define WALK_ACTION @"WALK_ACTION"
 
 @interface ZKPerson ()
 
@@ -17,45 +18,63 @@
 @property (strong, nonatomic) SKAction *upAction;
 @property (strong, nonatomic) SKAction *downAction;
 
+@property (strong, nonatomic) SKTexture *leftStanding;
+@property (strong, nonatomic) SKTexture *rightStanding;
+@property (strong, nonatomic) SKTexture *upStanding;
+@property (strong, nonatomic) SKTexture *downStanding;
+
+@property (assign, nonatomic) ZKWalkingDirection direction;
+
+@property (assign, nonatomic, getter = isWalking) BOOL walking;
+@property (strong, nonatomic) NSValue *nextTarget;
+
 @end
 
 @implementation ZKPerson
 
-+ (ZKPerson *)person {
-	SKTextureAtlas *manAtlas = [SKTextureAtlas atlasNamed:@"ManImages"];
+- (id)initWithPosition:(CGPoint)position {
+	if ((self = [super init])) {
+		SKTextureAtlas *manAtlas = [SKTextureAtlas atlasNamed:@"ManImages"];
+		
+		SKTexture *left1 = [manAtlas textureNamed:@"left_1"];
+		SKTexture *left2 = [manAtlas textureNamed:@"left_2"];
+		SKTexture *left3 = [manAtlas textureNamed:@"left_3"];
+		
+		SKTexture *right1 = [manAtlas textureNamed:@"right_1"];
+		SKTexture *right2 = [manAtlas textureNamed:@"right_2"];
+		SKTexture *right3 = [manAtlas textureNamed:@"right_3"];
+		
+		SKTexture *up1 = [manAtlas textureNamed:@"back_1"];
+		SKTexture *up2 = [manAtlas textureNamed:@"back_2"];
+		SKTexture *up3 = [manAtlas textureNamed:@"back_3"];
+		
+		SKTexture *down1 = [manAtlas textureNamed:@"front_1"];
+		SKTexture *down2 = [manAtlas textureNamed:@"front_2"];
+		SKTexture *down3 = [manAtlas textureNamed:@"front_3"];
+		
+		NSArray *walkLeftFrames = @[ left1, left2, left1, left3 ];
+		NSArray *walkRightFrames = @[ right1, right2, right1, right3 ];
+		NSArray *walkUpFrames = @[ up1, up2, up1, up3 ];
+		NSArray *walkDownFrames = @[ down1, down2, down1, down3 ];
+		
+		self.texture = left1;
+		self.size = left1.size;
+		
+		self.leftStanding = left1;
+		self.rightStanding = right1;
+		self.upStanding = up1;
+		self.downStanding = down1;
+		
+		CGFloat time = 0.1;
+		self.leftAction = [SKAction repeatActionForever:[SKAction animateWithTextures:walkLeftFrames timePerFrame:time resize:NO restore:YES]];
+		self.rightAction = [SKAction repeatActionForever:[SKAction animateWithTextures:walkRightFrames timePerFrame:time resize:NO restore:YES]];
+		self.upAction = [SKAction repeatActionForever:[SKAction animateWithTextures:walkUpFrames timePerFrame:time resize:NO restore:YES]];
+		self.downAction = [SKAction repeatActionForever:[SKAction animateWithTextures:walkDownFrames timePerFrame:time resize:NO restore:YES]];
+		
+		self.anchorPoint = CGPointMake(0.5, 0);
+	}
 	
-	SKTexture *left1 = [manAtlas textureNamed:@"left_1"];
-	SKTexture *left2 = [manAtlas textureNamed:@"left_2"];
-	SKTexture *left3 = [manAtlas textureNamed:@"left_3"];
-	
-	SKTexture *right1 = [manAtlas textureNamed:@"right_1"];
-	SKTexture *right2 = [manAtlas textureNamed:@"right_2"];
-	SKTexture *right3 = [manAtlas textureNamed:@"right_3"];
-	
-	SKTexture *up1 = [manAtlas textureNamed:@"back_1"];
-	SKTexture *up2 = [manAtlas textureNamed:@"back_2"];
-	SKTexture *up3 = [manAtlas textureNamed:@"back_3"];
-	
-	SKTexture *down1 = [manAtlas textureNamed:@"front_1"];
-	SKTexture *down2 = [manAtlas textureNamed:@"front_2"];
-	SKTexture *down3 = [manAtlas textureNamed:@"front_3"];
-	
-	ZKPerson *person = [[ZKPerson alloc] init];
-	
-	NSArray *walkLeftFrames = @[ left1, left2, left1, left3 ];
-	NSArray *walkRightFrames = @[ right1, right2, right1, right3 ];
-	NSArray *walkUpFrames = @[ up1, up2, up1, up3 ];
-	NSArray *walkDownFrames = @[ down1, down2, down1, down3 ];
-	
-	person.texture = left1;
-	person.size = left1.size;
-	
-	person.leftAction = [SKAction repeatActionForever:[SKAction animateWithTextures:walkLeftFrames timePerFrame:0.5 resize:NO restore:YES]];
-	person.rightAction = [SKAction repeatActionForever:[SKAction animateWithTextures:walkRightFrames timePerFrame:0.5 resize:NO restore:YES]];
-	person.upAction = [SKAction repeatActionForever:[SKAction animateWithTextures:walkUpFrames timePerFrame:0.5 resize:NO restore:YES]];
-	person.downAction = [SKAction repeatActionForever:[SKAction animateWithTextures:walkDownFrames timePerFrame:0.5 resize:NO restore:YES]];
-	
-	return person;
+	return self;
 }
 
 - (SKAction *)actionForDirection:(ZKWalkingDirection)direction {
@@ -67,9 +86,18 @@
 	}
 }
 
+- (SKTexture *)standingTextureForDirection:(ZKWalkingDirection)direction {
+	switch (direction) {
+		case ZKWalkingLeft: return self.leftStanding; break;
+		case ZKWalkingRight: return self.rightStanding; break;
+		case ZKWalkingUp: return self.upStanding; break;
+		case ZKWalkingDown: return self.downStanding; break;
+	}
+}
+
 - (void)startWalking {
 	if ([self actionForKey:MOVE_ACTION]) {
-		[self removeActionForKey:MOVE_ACTION];
+		return;
 	}
 	
 	[self runAction:[self actionForDirection:self.direction] withKey:MOVE_ACTION];
@@ -77,6 +105,36 @@
 
 - (void)stopWalking {
 	[self removeActionForKey:MOVE_ACTION];
+}
+
+- (void)walkTo:(CGPoint)target {
+	if (self.walking) {
+		self.nextTarget = [NSValue valueWithCGPoint:target];
+		return;
+	}
+	
+	self.walking = YES;
+	
+	CGFloat angle = atan2f(target.y - self.position.y, target.x - self.position.x);
+	angle = angle * (4.0 / (2 * M_PI));
+	if (angle < 0) angle += 4;
+	if (angle >= 4) angle -= 4;
+	self.direction = round(angle);
+	
+	if ((int)self.direction == 4) self.direction = 0;
+	
+	[self runAction:[SKAction moveTo:target duration:2] completion:^{
+		self.texture = [self standingTextureForDirection:self.direction];
+		[self stopWalking];
+		
+		self.walking = NO;
+		if (self.nextTarget) {
+			[self walkTo:[self.nextTarget CGPointValue]];
+			self.nextTarget = nil;
+		}
+	}];
+	
+	[self startWalking];
 }
 
 @end
